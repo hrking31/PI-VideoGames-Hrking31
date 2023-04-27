732c2c0c07e0ffcd26192dc18930dadf05a9cef6
Hernando Rey <hrking31@gmail.com>
2023-04-27 10:42:13 -0500

    Creacion Db

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,45 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaulvalue: UUIDV4,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      platforms: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      Image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      release: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      rating: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
